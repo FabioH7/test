6e314599baebf5e8896c74ec737a1da3194403d3
FabioH7 <fhysollari4@gmail.com>
2023-03-21 09:58:49 +0100

    adding all

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKeyConstraint
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKeyConstraint(
+        'states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     # state = relationship("State", back_populates="states")
