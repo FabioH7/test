538d6e02f90c4ba661745167d2ae0f203481a7de
FabioH7 <fhysollari4@gmail.com>
2023-03-21 11:14:07 +0100

    adding all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -41,6 +41,7 @@ class DBStorage:
         # name = DBStorage.tables[cls]/
         all_obj = self.__session.query(cls).all()
         for obj in all_obj:
+            print(obj)
             index = obj.__class__.__name__ + '.' + obj.id
             new_dict[index] = obj
         return new_dict
