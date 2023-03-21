042454bacb7d232dd846c3a25893cbea62baee93
FabioH7 <fhysollari4@gmail.com>
2023-03-21 10:39:43 +0100

    adding all

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                        value, '%Y-%m-%dT%H:%M:%S')
                 if key != '__class__':
                     setattr(self, key, value)
 
