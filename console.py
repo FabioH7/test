6d9dda3b3386fd60797cdf714ddf8830cbff3bee
FabioH7 <fhysollari4@gmail.com>
2023-03-21 11:07:59 +0100

    adding all

--- console.py
+++ console.py
@@ -199,7 +199,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
 
-        print(storage.all(c_name))
+        print(storage.all(c_name)[key])
         try:
             storage.delete(storage.all(c_name)[key])
             storage.save()
