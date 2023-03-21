d3c27e4a5fa2d24c96a22220a8727f9a1f215338
FabioH7 <fhysollari4@gmail.com>
2023-03-21 10:14:48 +0100

    adding all

--- console.py
+++ console.py
@@ -200,7 +200,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del (storage.all()[key])
+            del (storage.all(c_name)[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
